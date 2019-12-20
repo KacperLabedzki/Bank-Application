d1f44e21e7401ff237745d98012950fcb6ca3f3c
kacper <kacebr@gmail.com>
2019-12-20 15:05:25 +0100

    add EmailValidator class

--- bank/src/main/java/com/bankapplication/bank/validators/EmailValidator.java
+++ bank/src/main/java/com/bankapplication/bank/validators/EmailValidator.java
@@ -1,16 +1,23 @@
 package com.bankapplication.bank.validators;
 
-public class EmailValidator implements Validator{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements Validator {
+
+    private static final String EMAIL_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
     private String email;
+    private Pattern pattern;
+    private Matcher matcher;
 
     public EmailValidator(String email) {
         this.email = email;
+        pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
     }
-
 
     @Override
     public boolean isValid() {
-
-        return false;
+        matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
