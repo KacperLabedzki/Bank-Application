aa38a47da7c4a2db769d4e2693fb4be7ea8cd810
kacper <kacebr@gmail.com>
2019-12-20 14:42:57 +0100

    add EmailValidator class

--- bank/src/main/java/com/bankapplication/bank/validators/EmailValidator.java
+++ bank/src/main/java/com/bankapplication/bank/validators/EmailValidator.java
@@ -1,4 +1,16 @@
 package com.bankapplication.bank.validators;
 
-public class EmailValidator {
+public class EmailValidator implements Validator{
+    private String email;
+
+    public EmailValidator(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public boolean isValid() {
+
+        return false;
+    }
 }
