1ceaf4836eb6a98750827ed39650e5da8e16d186
kacper <kacebr@gmail.com>
2019-12-23 12:28:16 +0100

    filled getcustomer test

--- bank/src/test/java/com/bankapplication/bank/service/CustomerServiceTest.java
+++ bank/src/test/java/com/bankapplication/bank/service/CustomerServiceTest.java
@@ -1,5 +1,6 @@
 package com.bankapplication.bank.service;
 
+import com.bankapplication.bank.model.Address;
 import com.bankapplication.bank.model.Customer;
 import com.bankapplication.bank.repository.CustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,31 +30,36 @@ class CustomerServiceTest {
     private CustomerRepository customerRepository;
 
     @BeforeEach
-    public  void init(){
-        customer1 = new Customer(((long) 1),"Ania","Kolek","70051749977","ania@gmail.com","724023123",null,null);
-        customer2 = new Customer(((long) 2),"Arek","Nowak","54011487255","nowak@gmail.com","543432754",null,null);
-        customer3 = new Customer(((long) 3),"Teresa","Kowalska","67111977397","kowalska@gmail.com","732521853",null,null);
+    public void init() {
+
+        customer1 = new Customer(((long) 1), "Ania", "Kolek", "70051749977", "ania@gmail.com", "724023123", (Date.valueOf("1998-01-21")), new Address((long) 1, "Klonowa", "Lodz", "32-100"));
+        customer2 = new Customer(((long) 2), "Arek", "Nowak", "54011487255", "nowak@gmail.com", "543432754", (Date.valueOf("1992-06-12")), new Address((long) 2, "Zielona", "Warszawa", "10-101"));
+        customer3 = new Customer(((long) 3), "Teresa", "Kowalska", "67111977397", "kowalska@gmail.com", "732521853", (Date.valueOf("1989-12-11")), new Address((long) 3, "Morska", "Gdynia", "52-253"));
 
         customers.add(customer1);
         customers.add(customer2);
         customers.add(customer3);
     }
+
     @Test
     void getAllCustomers() {
         when(customerRepository.findAll()).thenReturn(customers);
-        assertEquals(3,customerService.getAllCustomers().size());
+        assertEquals(3, customerService.getAllCustomers().size());
     }
 
     @Test
     void getCustomer() {
         when(customerRepository.findById(1)).thenReturn(java.util.Optional.of(customer1));
         Customer customer = customerService.getCustomer(1);
-        assertEquals(1,customer.getId());
-        assertEquals("Ania",customer.getFirstName());
-        assertEquals("Kolek",customer.getLastName());
-        assertEquals("70051749977",customer.getPesel());
-        assertEquals("ania@gmail.com",customer.getEmail());
-        assertEquals("724023123",customer.getPhoneNumber());
+
+        assertEquals(1, customer.getId());
+        assertEquals("Ania", customer.getFirstName());
+        assertEquals("Kolek", customer.getLastName());
+        assertEquals("70051749977", customer.getPesel());
+        assertEquals("ania@gmail.com", customer.getEmail());
+        assertEquals("724023123", customer.getPhoneNumber());
+        assertEquals(Date.valueOf("1998-01-21"), customer.getDateOfBirth());
+        assertEquals(new Address((long) 1, "Klonowa", "Lodz", "32-100"), customer.getAddress());
     }
 
     @Test
@@ -66,4 +73,5 @@ class CustomerServiceTest {
     @Test
     void deleteCustomer() {
     }
+
 }
