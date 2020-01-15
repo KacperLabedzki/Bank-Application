6ccce90b2510c7989f175539cad801cc897da768
kacper <kacebr@gmail.com>
2020-01-15 17:49:37 +0100

    Added Account class

--- bank/src/main/java/com/bankapplication/bank/model/Account.java
+++ bank/src/main/java/com/bankapplication/bank/model/Account.java
@@ -1,4 +1,37 @@
 package com.bankapplication.bank.model;
 
-public class Acount {
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    private String nrRef;
+    private String nrb;
+    private BigDecimal balance;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    @JsonBackReference
+    private Customer accountOwner;
+
+    public Account(long id, String nrRef, String nrb, BigDecimal balance,Customer accountOwner) {
+        this.id = id;
+        this.nrRef = nrRef;
+        this.nrb = nrb;
+        this.balance = balance;
+        this.accountOwner = accountOwner;
+    }
+
+    public Account() {
+    }
 }
